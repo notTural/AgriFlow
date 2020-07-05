16f6af570e34d63639cc10abffa2a8665526352d
ch4ki <abdulxaligov@gmail.com>
2020-07-05 11:44:21 +0400

    Changes

--- lib/Models/plant.dart
+++ lib/Models/plant.dart
@@ -3,10 +3,11 @@ class Plant {
   DateTime plantDate;
   int areaId;
   double bioConst;
-
+  
   Plant(this.typeId, this.plantDate, this.bioConst);
 
   int plantedDays() {
     return DateTime.now().difference(plantDate).inDays;
   }
+
 }
--- lib/Screens/report_screen.dart
+++ lib/Screens/report_screen.dart
@@ -17,37 +17,7 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
-  Future<void> test() async {
-    var input = [
-      [
-        0.45378151,
-        0.31351351,
-        0.61016949,
-        0.27692308,
-        0.91176471,
-        0.12903226,
-        0.0,
-        0.35714286,
-        0.19548872,
-        0.43814433,
-        0.47368421
-      ]
-    ];
-    var output = List(1).reshape([1, 1]);
-    final interpreter = await Interpreter.fromAsset('model.tflite');
-
-    interpreter.run(input, output);
-    print('Neural Network Result:');
-    print(output);
-  }
-
-  void _loadModel() async {
-    // Creating the interpreter using Interpreter.fromAsset
-
-    final interpreter = await Interpreter.fromAsset("model.tflite");
-    print('Interpreter loaded successfully');
-  }
-
+  
 /*   Future<File> getFile(String fileName) async {
     final appDir = await getTemporaryDirectory();
     final appPath = appDir.path;
